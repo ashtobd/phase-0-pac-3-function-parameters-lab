18ab51e6acfb243d7f6f4aba1ad61be8c8461ca2
Brian Ashton <ashtobd@gmail.com>
2023-05-04 10:01:40 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name = "Brian"){
+    return(`Hi, my name is ${name}.`)
+}
+function introductionWithLanguage(name = "Brian", language = "JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+function introductionWithLanguageOptional(name = "Brian", language = "JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
